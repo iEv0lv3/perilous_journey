a83fd2a4a3ea75d15a2c599ee90e22302d173cab
Jordan Williams <jordan.wallace.williams@gmail.com>
2019-11-26 12:34:16 -0700

    Add logic for iteration 2 interaction pattern

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require './lib/node'
+require 'pry'
 
 class LinkedList
   attr_accessor :head
@@ -8,16 +9,47 @@ class LinkedList
   end
 
   def append(data)
-    @head = Node.new(data)
+    if @head == nil
+      @head = Node.new(data)
+    else
+      @head.next_node = Node.new(data)
+    end
   end
 
   def count
-    counter = []
-    counter << @head
-    counter.length
+    node_count = []
+    current_node = @head
+
+    while current_node.next_node != nil
+      node_count << current_node
+      current_node = current_node.next_node
+    end
+
+    node_count << @head
+    node_count.length
   end
 
   def to_string
-    "The #{head.surname} family"
+    node_surnames = []
+    current_node = @head
+    the_list = ""
+
+    while current_node.surname != nil
+      node_surnames << current_node.surname
+      if current_node.next_node != nil
+        current_node = current_node.next_node
+      else
+        break
+      end
+    end
+
+    node_surnames.each do |surname|
+      if surname == node_surnames[0]
+        the_list.concat("The #{surname} family")
+      else
+        the_list.concat(", followed by the #{surname} family")
+      end
+    end
+    the_list
   end
 end
