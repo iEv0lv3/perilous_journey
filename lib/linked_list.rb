d2ed7b7c7bc1a633e7a6216654b71c35deec69c1
Jordan Williams <jordan.wallace.williams@gmail.com>
2019-11-27 02:00:18 -0700

    Add logic to create find method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -41,6 +41,31 @@ class LinkedList
     node_count[(index - 1)].next_node = node
   end
 
+  def find(index, length)
+    node_count = []
+    current_node = @head
+    families = ""
+
+    while current_node.next_node != nil
+      node_count << current_node
+      current_node = current_node.next_node
+    end
+    node_count << @head
+    
+    count = 1
+    length.times do
+      if families.empty?
+        families.concat("The #{node_count[index].surname} family")
+        binding.pry
+      else
+        families.concat(", followed by the #{node_count[index + count].surname} family")
+        count += 1
+        binding.pry
+      end
+    end
+    families
+  end
+
   def count
     node_count = []
     current_node = @head
@@ -78,3 +103,12 @@ class LinkedList
     the_list
   end
 end
+
+list = LinkedList.new
+
+list.append('Brooks')
+list.append('Henderson')
+list.prepend('McKinney')
+list.insert(1, 'Lawson')
+
+binding.pry
