93382f1ec6240c0dc7601250d60dbce8dbbbc895
Jordan Williams <jordan.wallace.williams@gmail.com>
2019-11-29 19:34:46 -0700

    Add and update test logic for find method

--- test/wagon_train_test.rb
+++ test/wagon_train_test.rb
@@ -54,4 +54,14 @@ class WagonTrainTest < Minitest::Test
 
     assert_equal 'Williams', wt.list.head.next_node.surname
   end
+
+  def test_find_in_wagon_train
+    wt = WagonTrain.new
+    wt.append('Burke', 'pounds of food' => 200)
+    wt.prepend('West', 'spare wagon tongues' => 3)
+    wt.insert(1, 'Williams', 'plant seeds' => 153)
+    wt.prepend('Magnus', 'velociraptors' => 2)
+
+    assert_equal 'The Magnus family, followed by the West family', wt.find(0, 2)
+  end
 end
